64e4fa78e4bd2bc283d10df0c82d3475cfd2adf3
farzana11 <sssathi.cste@gail.com>
2015-01-15 11:51:26 +0600

    place

---
+++ places.php
@@ -0,0 +1,42 @@
+<?php
+
+
+$places=array(
+    array('city'=>'Tokyo',
+        'country'=>'Japan',
+        'continent'=>'Asia'),
+    array('city'=>'Mexico city',
+        'country'=>'Mexico',
+        'continent'=>'North America'),
+    array('city'=>'New York',
+        'country'=>'USA',
+        'continent'=>'North America'),
+    array('city'=>'Mumbai',
+        'country'=>'India',
+        'continent'=>'Asia'),
+    array('city'=>'Seoul',
+        'country'=>'Koria',
+        'continent'=>'Asia'),
+    array('city'=>'Tokyo',
+        'country'=>'Japan',
+        'continent'=>'Asia'),
+    array('city'=>'Shaughai',
+        'country'=>'China',
+        'continent'=>'Asia'),
+    array('city'=>'Logos',
+        'country'=>'Naijiria',
+        'continent'=>'Africa'),
+    array('city'=>'London',
+        'country'=>'UK',
+        'continent'=>'Europe'),
+
+);
+print_r ($places);
+echo $places[3];
+echo $places[3]['continent'];
+echo $places[3]['country']='bangladesh';
+echo $places[3]['city'];
+echo $places[3]['country'];
+
+
+    ?>
